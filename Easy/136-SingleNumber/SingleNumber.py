bf9761c23f384779dec6b26737f851e970318f38
Serhat Bekir Tan <serhatbekirtan@gmail.com>
2024-07-03 22:13:18 +0300

    Add SingleNumber

---
+++ Easy/136-SingleNumber/SingleNumber.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        single = 0
+
+        for num in nums:
+            single = single ^ num
+
+        return single
