0fbec0aff27150523766bc98a78432d6f588bfed
Serhat Bekir Tan <serhatbekirtan@gmail.com>
2024-07-03 19:49:09 +0300

    Add BestTimetoBuyandSellStock

---
+++ Easy/121-BestTimetoBuyandSellStock/BestTimetoBuyandSellStock.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+
+        min_buy = prices[0]
+
+        for i in range(len(prices)):
+            if prices[i] < min_buy:
+                min_buy = prices[i]
+            
+            profit = prices[i] - min_buy
+
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
