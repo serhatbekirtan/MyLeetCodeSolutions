1c87324f6f9fd94d1ccfa7560e804002ff844a65
Serhat Bekir Tan <serhatbekirtan@gmail.com>
2024-07-05 03:09:58 +0300

    Add MajorityElement

---
+++ Easy/169-MajorityElement/MajorityElement.py
@@ -0,0 +1,31 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = Counter(nums)
+
+        for num in nums:
+            if hashmap[num] > (len(nums)/2):
+                return num
+
+        
+    def majorityElementConstantSpace(self, nums: List[int]) -> int:
+        # Boyer-Moore Majority Voting Algorithm
+        count = 0
+        result = 0
+
+        for num in nums:
+            if num == result:
+                result = num
+                count += 1
+
+            if num != result and (count - 1) < 0:
+                result = num
+                count += 1
+
+            if num != result:
+                count -= 1
+
+        return result
