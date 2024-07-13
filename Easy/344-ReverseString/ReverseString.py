56eadde7db09260cb9593483feaf9446efa93991
Serhat Bekir Tan <serhatbekirtan@gmail.com>
2024-07-13 21:48:50 +0300

    Add ReverseString

---
+++ Easy/344-ReverseString/ReverseString.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+
+        j = len(s) - 1
+
+        for i in range(len(s)):
+            if i > j:
+                return
+
+            temp = s[i]
+            s[i] = s[j]
+            s[j] = temp
+
+            j -= 1
