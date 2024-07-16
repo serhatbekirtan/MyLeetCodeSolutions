f9a4f5289d47982f02a945798beedcddc19a49db
Serhat Bekir Tan <serhatbekirtan@gmail.com>
2024-07-16 19:49:48 +0300

    Add BinarySearch

---
+++ Easy/704-BinarySearch/BinarySearch.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        L = 0
+        R = len(nums) - 1
+
+        while L <= R:
+            mid = (L + R) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] > target:
+                R = mid - 1
+
+            if nums[mid] < target:
+                L = mid + 1
+
+        return -1
