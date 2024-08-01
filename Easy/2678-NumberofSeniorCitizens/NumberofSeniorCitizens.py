ee7b76b90171ccb8d3001e18fec9acba987fc9aa
Serhat Bekir Tan <serhatbekirtan@gmail.com>
2024-08-01 04:28:40 +0300

    Add NumberofSeniorCitizens

---
+++ Easy/2678-NumberofSeniorCitizens/NumberofSeniorCitizens.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        result = 0
+        
+        for detail in details:
+            if int(detail[11:13]) > 60:
+                result += 1
+
+        return result
