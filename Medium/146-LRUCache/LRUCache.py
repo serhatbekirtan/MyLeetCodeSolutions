903e1d7d8b9fa728bcbd201f9d4cbaeeb0e3f822
Serhat Bekir Tan <serhatbekirtan@gmail.com>
2024-08-08 19:53:36 +0300

    Add LRUCache

---
+++ Medium/146-LRUCache/LRUCache.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {} # {key: Node}
+
+        # left = LRU, right = MRU
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    # Remove node from linked list
+    def removeNode(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    # Insert node before rightmost node
+    def insertNode(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.removeNode(self.cache[key])
+            self.insertNode(self.cache[key])
+            return self.cache[key].val
+        
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insertNode(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            # Remove from the list and delete the LRU from the hashmap
+            lru = self.left.next
+            self.removeNode(lru)
+            del self.cache[lru.key]
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
