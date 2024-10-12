e6fdb320422cd32ad14b209746a9d7ecb5750d45
Serhat Bekir Tan <serhatbekirtan@gmail.com>
2024-10-12 18:03:44 +0300

    Update MinimumAddtoMakeParanthesesValid

--- Medium/921-MinimumAddtoMakeParanthesesValid/MinimumAddtoMakeParanthesesValid.py
+++ Medium/921-MinimumAddtoMakeParanthesesValid/MinimumAddtoMakeParanthesesValid.py
@@ -1,5 +1,16 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
+        stack = []
+        for c in s:
+            stack.append(c)
+            if len(stack) > 1 and (stack[-1] == ')' and stack[-2] == '('):
+                stack.pop()
+                stack.pop()
+        
+        return len(stack)      
+    
+    
+    def minAddToMakeValid(self, s: str) -> int:
         open_c = close_c = 0
         for c in s:
             if c == '(':
